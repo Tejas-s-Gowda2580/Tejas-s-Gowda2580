acf9e745f5fc97d48f31366440b7e9651f31e506
tejas <tejasgowdaurt@gmail.com>
2024-06-13 02:17:34 +0530

    i added the internship file

--- intership/python.py
+++ intership/python.py
@@ -114,3 +114,104 @@ for x in range(5,0,-1):
     for z in range(x,0,-1):
         print(z+" ")
     print()
+
+############################################## given sorted array count the numbers
+#
+arr=[1,2,2,4,5]
+x=int(input("enter the no. to count in array"))
+count=0
+for i in range(len(arr)):
+    if arr[i]==x:
+        count=count+1
+print("no. of occurences: ",count)
+
+###########################################addition of 2 no.s without add symbol and arithmatic
+a=int(input("enter the  value of a:"))
+b=int(input("enter the  value of b:"))
+sum=a-(-(b))
+print("sum:",sum)
+
+######################################### write prgm to find modulus without using modulus
+a=int(input("enter the  value of a:"))
+b=int(input("enter the  value of b:"))
+q=a//b
+# if part may not ne needed in many cases
+if q<0:
+    q+=1
+m=a-b*q
+print(m)
+
+##################################### divide without using the divide symbol and modulus
+a=float(input("enter the  value of a:"))
+b=float(input("enter the  value of b:"))
+count=0
+while b!=0:
+    if b<a:
+        a=a-b
+        count=count+1
+    else:
+        break
+print(count)
+
+#################################### finding the biggest prime factorial
+import math
+def maxPrimeFactor(n):
+    max_Prime = 1
+    while n % 2 == 0:
+        max_Prime = 2
+        n /= 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            max_Prime = i
+            n = n / i
+    if n > 2:
+        max_Prime = n
+    return int(max_Prime)
+
+n = 25
+print(maxPrimeFactor(n))
+
+############################### finding the missing values
+n=7
+arr=[7,5,3,2,1]
+for i in range(1,n+1):
+    if i not in arr:
+        print("missing number",i)
+#
+#
+################################## another method for finding missing values(by taking input from user)
+from array import array
+
+n = int(input('Enter the number: '))
+
+if not (1 <= n <= 106):
+    print("Invalid input. Please enter a number within the range [1, 10^6].")
+else:
+    arr = array('i', [])
+    print("Enter the array elements separated by spaces:")
+    arr.extend(map(int, input().split()))
+
+    for i in range(1, n + 1):
+        if i not in arr:
+            print("Missing number:", i)
+
+# ####################################### finding the second largest element
+def second_largest(arr):
+    arr=list(set(arr))
+    arr.sort()
+    return arr[-2]
+
+array = [14, 67, 8, 89]
+print(list(array))
+print("Second largest number is ",second_largest(array))
+
+
+# ######################################## finding the maximum difference
+def maximum_diff(arr):
+    arr=list(set(arr))
+    arr.sort()
+    return (arr[-1]-arr[0])
+
+array=[3,4,1,9,56,7,9,12]
+print(list(array))
+print("largest difference is ",maximum_diff(array))
